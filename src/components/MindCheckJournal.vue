66ee297cf2f9c66c9ae572b66a12f4e0f7ae37a8
evamose <yg040605@gmail.com>
2025-09-08 01:20:32 +0900

    버튼 및 수정사항 완료, 캡쳐 기능 테스트 예정

--- src/components/MindCheckJournal.vue
+++ src/components/MindCheckJournal.vue
@@ -1,122 +1,125 @@
 <template>
-  <div id="app">
-    <div id="journal-content-to-capture">
-      <h1>하루톡</h1>
+  <div class="my-text">
+  <div>
+    <header class="header">
+      <div class="header-content">
+      <BackButton :to="'/CardNewsMain'"/>
+      <p class="page-title">오늘의 일지</p>
+      </div>
+    </header>
 
-      <div class="header-section" style="text-align: center;">
+    <div class="content"><!--캡쳐 시작지점-->
+      <div class="intro-card">
         <p>
           <strong>나의 하루를 한 문장으로 표현해주세요.</strong>
         </p>
         <p>
-          <textarea
-              id="entryMood"
-              v-model="entryMood"
-              maxlength="20"
-              rows="1"
-              ref="myAutoGrowTextarea"
-              @input="adjustTextareaHeight"
-              style="width: 100%; resize: vertical; white-space: pre-wrap; word-wrap: break-word;"
-              placeholder="오늘은 행복한 날이예요!"
-          ></textarea>
+             <textarea
+                 id="entryMood"
+                 v-model="entryMood"
+                 maxlength="20"
+                 rows="1"
+                 ref="myAutoGrowTextarea"
+                 @input="adjustTextareaHeight"
+                 placeholder="오늘은 행복한 날이예요!"
+                 class="textarea-style"
+             ></textarea>
         </p>
       </div>
-
-      <div class="lifestyle-checklist">
-        <h2 class="checklist-title">나의 라이프스타일 체크리스트</h2>
-
-        <div class="flex-container">
-        <div class="checklist-item">
-          <strong>1. 오늘의 기분</strong>
-          <div class="radio-group">
-            <label><input type="radio" name="mood" value="best" v-model="checklist.mood" /> <span class="radio-circle"></span>매우 좋아요</label>
-            <label><input type="radio" name="mood" value="good" v-model="checklist.mood" /><span class="radio-circle"></span> 좋아요</label>
-            <label><input type="radio" name="mood" value="soso" v-model="checklist.mood" /><span class="radio-circle"></span>그저그래요</label>
-            <label><input type="radio" name="mood" value="bad" v-model="checklist.mood" /><span class="radio-circle"> </span>나빠요</label>
-            <label><input type="radio" name="mood" value="vlevle" v-model="checklist.mood" /> <span class="radio-circle"></span>매우 나빠요</label>
+      <h2 class="h2-text">나의 라이프스타일 체크리스트</h2>
+      <div>
+        <div class="check-list">
+          <strong>1. 오늘의 기분은? </strong>
+          <div class="rating-container">
+            <label class="rating-option custom-radio"><input type="radio" name="mood" value="best" v-model="checklist.mood" /> <span class="btn-text">매우 좋아요</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="mood" value="good" v-model="checklist.mood" /> <span class="btn-text">좋아요</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="mood" value="soso" v-model="checklist.mood" /> <span class="btn-text">그저 그래요</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="mood" value="bad" v-model="checklist.mood" /> <span class="btn-text">나빠요</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="mood" value="vlevle" v-model="checklist.mood" /> <span class="btn-text">매우 나빠요</span> </label>
           </div>
         </div>
 
-        <div class="checklist-item">
+        <div class="check-list">
           <strong>2. 오늘 수분 섭취량</strong>
-          <div class="radio-group">
-            <label><input type="radio" name="water" value="1cup" v-model="checklist.water" /><span class="radio-circle"></span> 1컵</label>
-            <label><input type="radio" name="water" value="2_4cup" v-model="checklist.water" /><span class="radio-circle"></span> 2~4컵</label>
-            <label><input type="radio" name="water" value="5_7cup" v-model="checklist.water" /><span class="radio-circle"></span> 5~7컵</label>
-            <label><input type="radio" name="water" value="8_10cup" v-model="checklist.water" /><span class="radio-circle"></span> 8~10컵</label>
-            <label><input type="radio" name="water" value="11cup" v-model="checklist.water" /><span class="radio-circle"></span> 11컵 이상</label>
+          <div class="rating-container">
+            <label class="rating-option custom-radio"><input type="radio" name="water" value="1cup" v-model="checklist.water" /> <span class="btn-text">1컵 </span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="water" value="2\_4cup" v-model="checklist.water" /> <span class="btn-text">2~4컵 </span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="water" value="5\_7cup" v-model="checklist.water" /> <span class="btn-text">5~7컵 </span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="water" value="8\_10cup" v-model="checklist.water" /> <span class="btn-text">8~10컵 </span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="water" value="11cup" v-model="checklist.water" /> <span class="btn-text">11컵 이상 </span></label>
           </div>
         </div>
 
-        <div class="checklist-item">
+        <div class="check-list">
           <strong>3. 오늘 수면시간</strong>
-          <div class="radio-group">
-            <label><input type="radio" name="hour" value="4hour" v-model="checklist.hour" /><span class="radio-circle"></span> 4시간 이하</label>
-            <label><input type="radio" name="hour" value="5_6hour" v-model="checklist.hour" /><span class="radio-circle"></span> 5~6시간</label>
-            <label><input type="radio" name="hour" value="7_8hour" v-model="checklist.hour" /><span class="radio-circle"></span> 7~8시간</label>
-            <label><input type="radio" name="hour" value="9_10hour" v-model="checklist.hour" /><span class="radio-circle"></span> 9~10시간</label>
-            <label><input type="radio" name="hour" value="10hour" v-model="checklist.hour" /><span class="radio-circle"></span> 10시간 이상</label>
+          <div class="rating-container">
+            <label class="rating-option custom-radio"><input type="radio" name="hour" value="4hour" v-model="checklist.hour" /> <span class="btn-text">4시간 이하</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="hour" value="5\_6hour" v-model="checklist.hour" /> <span class="btn-text">5~6시간</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="hour" value="7\_8hour" v-model="checklist.hour" /> <span class="btn-text">7~8시간</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="hour" value="9\_10hour" v-model="checklist.hour" /> <span class="btn-text">9~10시간</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="hour" value="10hour" v-model="checklist.hour" /> <span class="btn-text">10시간 이상</span> </label>
           </div>
         </div>
 
-        <div class="checklist-item">
+        <div class="check-list">
           <strong>4. 규칙적인 식사 (중복 선택 가능)</strong>
-          <div class="checkbox-group">
-            <label><input type="checkbox" value="morning" v-model="checklist.meals" /><span class="checkbox-box"></span> 아침</label>
-            <label><input type="checkbox" value="afternoon" v-model="checklist.meals" /><span class="checkbox-box"></span> 점심</label>
-            <label><input type="checkbox" value="evening" v-model="checklist.meals" /><span class="checkbox-box"></span> 저녁</label>
-            <label><input type="checkbox" value="nothing" v-model="checklist.meals" /><span class="checkbox-box"></span> 아무것도 먹지 않음</label>
+          <div class="rating-container">
+            <label class="custom-checkbox-btn"><input type="checkbox" value="morning" v-model="checklist.meals" /> <span class="btn-text">아침</span> </label>
+            <label class="custom-checkbox-btn"><input type="checkbox" value="afternoon" v-model="checklist.meals" /> <span class="btn-text">점심</span> </label>
+            <label class="custom-checkbox-btn"><input type="checkbox" value="evening" v-model="checklist.meals" /> <span class="btn-text">저녁</span> </label>
+            <label class="custom-checkbox-btn"><input type="checkbox" value="nothing" v-model="checklist.meals" /> <span class="btn-text">아무것도 먹지 않음</span> </label>
           </div>
         </div>
 
-        <div class="checklist-item">
+        <div class="check-list">
           <strong>5. 하루 중 취미 생활을 30분 이상 했는가?</strong>
-          <div class="radio-group">
-            <label><input type="radio" name="enjoyableActivity" value="yes" v-model="checklist.enjoyableActivity" /><span class="radio-circle"></span> 그렇다</label>
-            <label><input type="radio" name="enjoyableActivity" value="no" v-model="checklist.enjoyableActivity" /><span class="radio-circle"></span> 아니다</label>
+          <div class="rating-container">
+            <label class="rating-option custom-radio"><input type="radio" name="enjoyableActivity" value="yes" v-model="checklist.enjoyableActivity" /> <span class="btn-text">그렇다</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="enjoyableActivity" value="no" v-model="checklist.enjoyableActivity" /> <span class="btn-text">아니다</span> </label>
           </div>
         </div>
 
-        <div class="checklist-item">
+        <div class="check-list">
           <strong>6. 하루 중 운동을 30분 이상 했나요?</strong>
-          <div class="radio-group">
-            <label><input type="radio" name="exercise" value="yes" v-model="checklist.exercise" /><span class="radio-circle"></span> 그렇다</label>
-            <label><input type="radio" name="exercise" value="no" v-model="checklist.exercise" /><span class="radio-circle"></span> 아니다</label>
+          <div class="rating-container">
+            <label class="rating-option custom-radio"><input type="radio" name="exercise" value="yes" v-model="checklist.exercise" /> <span class="btn-text">그렇다</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="exercise" value="no" v-model="checklist.exercise" /> <span class="btn-text">아니다</span> </label>
           </div>
         </div>
 
-        <div class="checklist-item">
+        <div class="check-list">
           <strong>7. 하루 중 누군가와 마음을 나눈 경험이 있나요?</strong>
-          <div class="radio-group">
-            <label><input type="radio" name="heartSharing" value="yes" v-model="checklist.heartSharing" /><span class="radio-circle"></span> 그렇다</label>
-            <label><input type="radio" name="heartSharing" value="no" v-model="checklist.heartSharing" /><span class="radio-circle"></span> 아니다</label>
+          <div class="rating-container">
+            <label class="rating-option custom-radio"><input type="radio" name="heartSharing" value="yes" v-model="checklist.heartSharing" /> <span class="btn-text">그렇다</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="heartSharing" value="no" v-model="checklist.heartSharing" /> <span class="btn-text">아니다</span> </label>
           </div>
         </div>
 
-        <div class="checklist-item">
+        <div class="check-list">
           <strong>8. 하루 중 친절을 베푼 경험이 있나요?</strong>
-          <div class="radio-group">
-            <label><input type="radio" name="kindness" value="yes" v-model="checklist.kindness" /><span class="radio-circle"></span> 그렇다</label>
-            <label><input type="radio" name="kindness" value="no" v-model="checklist.kindness" /><span class="radio-circle"></span> 아니다</label>
+          <div class="rating-container">
+            <label class="rating-option custom-radio"><input type="radio" name="kindness" value="yes" v-model="checklist.kindness" /> <span class="btn-text">그렇다</span> </label>
+            <label class="rating-option custom-radio"><input type="radio" name="kindness" value="no" v-model="checklist.kindness" /> <span class="btn-text">아니다</span> </label>
           </div>
-        </div>
         </div>
       </div>
     </div>
-    <div class="flex-center-x" style="margin-top: 1rem;">
-      <button class="button-item" @click="saveJournal">저장하기</button>
-    </div>
+
 
     <NavigationBar />
   </div>
+  </div>
 </template>
+
 
 <script>
 import NavigationBar from "@/components/NavigationBar.vue";
 import html2canvas from "html2canvas";
+import BackButton from "@/components/BackButton.vue";
 
 export default {
   name: "MindCheckJournal",
-  components: { NavigationBar },
+  components: {BackButton, NavigationBar },
   data() {
     return {
       entryDate: "",
